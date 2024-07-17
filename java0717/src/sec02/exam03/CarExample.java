14e0fb1cb0f867326c26f1701896a42cc1e41ba4
Kimcy0920 <qkqhzmfjddl@naver.com>
2024-07-17 17:07:07 +0900

    0717

--- java0717/src/sec02/exam03/CarExample.java
+++ java0717/src/sec02/exam03/CarExample.java
@@ -6,25 +6,27 @@ public class CarExample {
 		Car car = new Car();
 		
 		for(int i=1; i<=5; i++) {
-			int problemLocation = car.run();
+			TireLoc problemLocation = car.run();
 			
 			switch(problemLocation) {
-			case 1:
+			case FrontLeftTire:
 				System.out.println("앞왼쪽 HankookTire로 교체");
 				car.frontLeftTire = new HankookTire("앞왼쪽", 15);
 				break;
-			case 2:
+			case FrontRightTire:
 				System.out.println("앞오른쪽 KumhoTire로 교체");
 				car.frontRightTire = new HankookTire("앞오른쪽", 13);
 				break;
-			case 3:
+			case BackLeftTire:
 				System.out.println("뒤왼쪽 HankookTire로 교체");
 				car.backLeftTire = new HankookTire("뒤왼쪽", 14);
 				break;
-			case 4:
+			case BackRightTire:
 				System.out.println("뒤오른쪽 KumhoTire로 교체");
 				car.backRightTire = new HankookTire("뒤오른쪽", 17);
 				break;
+			case NoProblem:
+				System.out.println("몬다이나이");
 			}
 			System.out.println("--------------------------");
 		}
